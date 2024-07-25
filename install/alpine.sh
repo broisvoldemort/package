b3a0302d39cfb859c9ab54f0057dd46ba446d305
broisvoldemort <149794095+broisvoldemort@users.noreply.github.com>
2024-07-25 17:50:54 +0300

    Create alpine.sh

---
+++ install/alpine.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo Installing requirements...
+apk add --no-cache wget
+
+wget -O /usr/local/bin/package https://github.com/broisvoldemort/package/raw/master/scripts/alpine/package.sh
+if [ $? -eq 0 ]; then
+  chmod +x /usr/local/bin/package
+  echo "Installed successfully."
+else
+  echo "Failed to download the package script."
+  exit 1
+fi
+
+rm -f "$0"
